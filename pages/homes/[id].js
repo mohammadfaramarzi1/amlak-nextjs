cf487ebdb26dcf3cbe64891995eef36082645a17
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-03-04 15:29:17 +0330

    init project

---
+++ pages/homes/[id].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingleHome() {
+  return (
+    <h1>SingleHome Page</h1>
+  )
+}
+
+export default SingleHome
---
+++ pages/homes/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Homes() {
+  return (
+    <h1>All Homes Page</h1>
+  )
+}
+
+export default Homes
