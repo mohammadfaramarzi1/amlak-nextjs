6f85c4d2ccd1e0a4382c69c84573d9d72df33c91
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2025-03-05 09:46:36 +0330

    feat: search home

--- pages/homes/index.js
+++ pages/homes/index.js
@@ -1,7 +1,19 @@
 import Home from "@/components/modules/Home";
 import db from "../../data/db.json";
+import { useEffect, useState } from "react";
 
 function Homes() {
+  const [search, setSearch] = useState("");
+  const [allHomes, setAllHomes] = useState(db.homes);
+  console.log(allHomes);
+
+  useEffect(() => {
+    const filteredHomes = db.homes.filter((home) =>
+      home.title.toLowerCase().includes(search)
+    );
+    setAllHomes(filteredHomes);
+  }, [search]);
+
   return (
     <div className="home-section" id="houses">
       <div className="home-filter-search">
@@ -17,31 +29,40 @@ function Homes() {
           </select>
         </div>
         <div className="home-search">
-          <input type="text" placeholder="جستجو کنید" />
+          <input
+            type="text"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+            placeholder="جستجو کنید"
+          />
         </div>
       </div>
       <div className="homes">
-        {db.homes.map((home) => (
-          <Home key={home.id} {...home} />
-        ))}
+        {allHomes.length ? (
+          allHomes.map((home) => <Home key={home.id} {...home} />)
+        ) : (
+          <p className="empty">املاکی با این نام وجود ندارد...</p>
+        )}
       </div>
-      <ul className="pagination__list">
-        <li className="pagination__item">
-          <a href="#" className="">
-            {" "}
-          </a>
-        </li>
-        <li className="pagination__item">
-          <a href="#" className="">
-            2
-          </a>
-        </li>
-        <li className="pagination__item active">
-          <a href="#" className="">
-            1
-          </a>
-        </li>
-      </ul>
+      {!!allHomes.length && (
+        <ul className="pagination__list">
+          <li className="pagination__item">
+            <a href="#" className="">
+              {" "}
+            </a>
+          </li>
+          <li className="pagination__item">
+            <a href="#" className="">
+              2
+            </a>
+          </li>
+          <li className="pagination__item active">
+            <a href="#" className="">
+              1
+            </a>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
